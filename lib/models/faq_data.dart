dacbf0e42ae6c178c93c209ddc0a507eb64f7a00
unknown <papai4517@gmail.com>
2024-09-05 17:46:13 +0530

    clickablequestion

--- lib/models/faq_data.dart
+++
@@ -1,7 +0,0 @@
-
-
-List<Map<String, String>> faqData = [
-  {"question": "What is Flutter?", "answer": "Flutter is an open-source UI toolkit created by Google."},
-  {"question": "How do I install Flutter?", "answer": "You can install Flutter from the official website: https://flutter.dev"},
-  // Add more FAQs here
-];
